ca7398228f98b3d4a5c9690f8de74abf249fbba3
Michael Laptop <p4inki77er@gmail.com>
2019-11-23 11:14:21 +0100

    test

--- ai/app/app.py
+++ ai/app/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request, send_file
+import requests
 from flask_cors import CORS
 from azure_requests import request_prediction
 from dotenv import load_dotenv
 import os
+import threading
 
 load_dotenv()
 
@@ -13,18 +15,37 @@ cors = CORS(app)
 endpoint = os.getenv('ENDPOINT')
 prediction_key = os.getenv('PREDICTION_KEY')
 
+
+class AsyncRequest(threading.Thread):
+    def __init__(self, filepath, id):
+        self.filepath = filepath
+        self.id = id
+
+    def run(self):
+        try:
+            cracked, uncracked = request_prediction(self.filepath, endpoint, prediction_key)
+            cracked = int(cracked * 100)
+            print(cracked)
+            url = "/images/" + self.id + "/probability"
+            data = jsonify({
+                "probability": cracked
+            })
+            return requests.post(url, data=data)
+
+        except:
+            return None
+
+
 @app.route('/model/predict', methods=['POST'])
-def model_predict():
+async def model_predict():
     r = request.get_json()
     print(str(r))
     filename = r["id"]
     filepath = "/data/images/" + filename + ".jpg"
-    cracked, uncracked = request_prediction(filepath, endpoint, prediction_key)
-    cracked = int(cracked * 100)
-    print(cracked)
-    return jsonify({
-        "probability": cracked
-    })
+    asnycRequest = AsyncRequest(filepath=filepath, id=filename)
+    asnycRequest.start()
+
+    return "Request sent"
 
 
 @app.route('/model/train', methods=['POST'])
