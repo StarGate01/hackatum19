51678291a61b1f3ca8d40f237c09213862a9bb77
Michael Laptop <p4inki77er@gmail.com>
2019-11-23 05:44:17 +0100

    Rest endpoints: - /model/predict - /model/train
    is working with mock values

--- ai/app/app.py
+++ ai/app/app.py
@@ -1,24 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import random
 
 app = Flask(__name__)
 app.config.from_object('config.DevelopmentConfig')
 cors = CORS(app)
 
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    return 'Hello World!'
-
-
 @app.route('/model/predict', methods=['POST'])
 def model_predict():
-    return "model predict"
+    r = request.get_json()
+    filepath = r["filepath"]
+
+    return jsonify({
+        "probability": random.randint(0, 99)
+    })
 
 
 @app.route('/model/train', methods=['POST'])
 def model_train():
-    return "model train"
+    r = request.get_json()
+    filepath = r["filepath"]
+    is_cracked = r["iscracked"]
+    return "Model trained"
 
 
 if __name__ == '__main__':
