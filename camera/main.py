6a2aad860b579273f4ab8b62fcb0ea7321f93985
Christoph Honal <christoph.honal@web.de>
2019-11-23 07:04:04 +0100

    camera first version done

--- camera/main.py
+++ camera/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-import sys, os
-import time
+import sys, os, time, requests
 from signal import *
 from os import listdir
 from os.path import isfile, join
@@ -10,11 +9,13 @@ imgs = []
 cidx = 0
 
 def trigger(img):
-    print("Taking and sending picture: " + img)
+    print("Taking and sending image: " + img)
+    imgfile = {"image": open("/data/mock/" + img, 'rb')}
+    res = requests.post("http://core:3000/images", files=imgfile)
+    print("Done sending image: " + str(res.status_code) + " " + res.text)
 
 def main():
     global cidx, imgs
-
     imgs = [f for f in listdir("/data/mock") if isfile(join("/data/mock", f))]
     print(str(len(imgs)) + " images found")
     print("Camera mock started\n")
@@ -26,10 +27,8 @@ def main():
             cidx = 0
         time.sleep(20)
 
-
 def sigusr(sig, frame):
     global cidx, imgs
-
     print("SIGUSR: cidx=" + str(cidx))
     trigger(imgs[cidx])
 
