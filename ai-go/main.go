1b77ac8363b98736bc5a1da14abd92b5c74c3afc
Marko Stapfner <marko@markostapfner.com>
2019-11-23 11:58:35 +0100

    für chrz einfach hoch mit der scheiße

---
+++ ai-go/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	// Enable Cors for the Frontend
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", " DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+	r.Use(cors.Handler)
+
+	r.Post("/model/predict", HandlePredictRequest)
+
+	r.Post("/model/train", HandleTrainRequest)
+
+	log.Println("Mattermost-Connector started")
+	http.ListenAndServe(":80", r)
+}
+
+type PredictRequest struct {
+	Id string `json:"id"`
+}
+
+type PredictResponse struct {
+	Probability int `json:"probability"`
+}
+
+func HandlePredictRequest(w http.ResponseWriter, r *http.Request) {
+	log.Println("Handle predict request")
+	var predictRequest PredictRequest
+	err := json.NewDecoder(r.Body).Decode(&predictRequest)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(predictRequest.Id)
+	log.Println("Before response to core")
+	go PredictResponseToCore(predictRequest.Id)
+	w.Write([]byte("Okay"))
+	return
+}
+
+func PredictResponseToCore(id string) {
+	log.Println("LOL")
+	time.Sleep(2000)
+	var predictResponse PredictResponse
+	predictResponse.Probability = 32
+
+	bytesRepresentation, err := json.Marshal(predictResponse)
+	if err != nil {
+		log.Println(err)
+	}
+
+	url := "http://core:3000/images/" + id + "/probability"
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(bytesRepresentation))
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println(resp.Status)
+}
+
+func HandleTrainRequest(w http.ResponseWriter, r *http.Request) {
+	log.Println("Handle train request ")
+}
--- ai/app/app.py
+++ ai/app/app.py
@@ -20,19 +20,17 @@ predction_resource_id = os.getenv("PREDICTION_RESOURCE_ID")
 @app.route('/model/predict', methods=['POST'])
 def model_predict():
     r = request.get_json()
-    print(str(r))
+    print("SAKJHDAKJHDKAJSHD")
+    print(r)
     filename = r["id"]
     filepath = "/data/images/" + filename + ".jpg"
     cracked, uncracked = request_prediction(filepath, endpoint, prediction_key)
-    cracked = int(cracked * 100)
-    print(cracked)
-    return jsonify({
-        "probability": cracked
-    })
+    return
 
 
 @app.route('/model/train', methods=['POST'])
 def model_train():
+    print("TRAIN")
     r = request.get_json()
     filename = r["id"]
     is_cracked = r["iscracked"]
