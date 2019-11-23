603c5875d0ec21392b7ae763bd757d9ade06f17b
Michael Laptop <p4inki77er@gmail.com>
2019-11-23 05:27:23 +0100

    basic rest endpoints configured

--- ai/app/app.py
+++ ai/app/app.py
@@ -11,5 +11,15 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/model/predict', methods=['POST'])
+def model_predict():
+    return "model predict"
+
+
+@app.route('/model/train', methods=['POST'])
+def model_train():
+    return "model train"
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
